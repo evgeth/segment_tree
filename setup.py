aaf5df10e9e36d8430777396e2dff353e396a7ba
Evgeny Yurtaev <eugene@yurtaev.me>
2017-12-12 22:15:15 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='segment_tree',
+      version='0.1',
+      description='The most comprehensive implementation of Segment Tree.',
+      url='https://github.com/evgeth/segment_tree',
+      author='Evgeny Yurtaev',
+      author_email='eugene@yurtaev.com',
+      license='MIT',
+      packages=['segment_tree'],
+      zip_safe=False)

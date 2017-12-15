99e2585c9cdc74a2fcb1b00cf8a61b25b71387fa
Evgeny Yurtaev <eugene@yurtaev.me>
2017-12-15 14:46:35 +0900

    README fetching fix

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 from setuptools import find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
